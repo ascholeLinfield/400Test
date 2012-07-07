1879681c4d76dda876e7d51b24d24f987c2fc434
Andrew Scholer <ascholer@ascholer.org>
2012-07-07 12:49:39 -0700

    Added App shell

---
+++ Application/Application/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        MessageBox.Show("It works")
+    End Sub
+End Class
